56d9f3467100ffc60506d6a5a96127db206e997b
TreverHibbs <treverhibbs@gmail.com>
2020-12-06 19:59:11 -0800

    Add dependencies
    Why: I need code on github for app to run in github pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var d3_1 = __importDefault(require("d3"));
+console.log("d3", d3_1.default);
 var helloWorld = function () {
     console.log('hello');
 };
+helloWorld();
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import * as d3 from "d3";
+console.log("d3", d3);
+
 let helloWorld = () => {
   console.log('hello');
 }
+
+helloWorld();
