d9de1f69cf374a5890898a3e21f30ea27eb11d62
TreverHibbs <treverhibbs@gmail.com>
2020-12-10 16:03:02 -0800

    Start the add node listener function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
 "use strict";
 
-const mainSvg = d3.select("main-svg");
-console.debug("mainSvg", mainSvg); 
+const myVertexSet = [];
+const myEdgeSet = [];
+
+const addVertex = (myEvent) => {
+  myVertexSet.push(myVertexSet.length+1);
+  console.debug("myVertexSet", myVertexSet);
+  const coordinates = d3.pointer(myEvent)
+  const newVertex = d3.select("g")
+
+  newVertex.append("circle")
+    .attr('cx', coordinates[0])
+    .attr('cy', coordinates[1])
+    .attr('r', 20)
+    .attr('fill', 'white')
+    .attr('stroke', 'black');
+  newVertex.append("text")
+    .attr('x', coordinates[0])
+    .attr('y', coordinates[1])
+    .attr('text-anchor', 'middle')
+    .style('color', 'white')
+    .html("hello");
+  
+  d3.select("#main").append(newVertex);
+}
+
+const mySvg = d3.select("#main")
+  .on("click", addVertex);
+
+
+//const myVertex = mySvg.data(myVertexSet)
+//  .data(myEdgeSet)
+//  .append("g");
+
+
+
+//myVertex.append("circle")
+//  .attr('cx', 50)
+//  .attr('cy', 50)
+//  .attr('r', 20);
+//
+//myVertex.append("text")
+//  .attr('x', "50")
+//  .attr('y', "50")
+//  .attr("text-anchor", "middle")
+//  .html("hello");
 
