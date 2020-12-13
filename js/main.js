42ea8612407424a8bd8829cbee44814b0837df42
TreverHibbs <treverhibbs@gmail.com>
2020-12-13 10:17:34 -0800

    Add working nodes and edges

--- js/main.js
+++ js/main.js
@@ -14,44 +14,50 @@ const colors = [
 ]
 
 //local selections
-const force = d3.forceSimulation();
+const nodes = [];
+const links = [];
+const force = d3.forceSimulation()
+  .force('link', d3.forceLink().distance(100));
 const mySvg = d3.select("#main");
-let node = mySvg.selectAll('.node');
+let vertices = mySvg.selectAll('.node');
+let edges = mySvg.selectAll('.link');
 
 //local state variables
-let addingEdge = 0;
+let selectedNodeIndex = null;
+
 
 //functions for dynamic interaction
 const addNode = (e) => {
-  const nodes = force.nodes();
-  //console.log("nodes", nodes);
+  console.log("nodes", nodes);
   const coordinates = d3.pointer(e);
-  //console.log("coord", coordinates);
+  console.log("coord", coordinates);
   const newNode = { x: coordinates[0], y: coordinates[1], id: nodes.length };
   nodes.push(newNode);
-  force.nodes(nodes);
   update();
-  //console.log(node);
 }
 
-const addEdge = (e) => {
-  if(addingEdge){
-    console.log("add edge");
-    addingEdge = 0;
-  }
+/**
+ * @desc A funtion that is called when a mouse drag is detected from another node
+ * @param e - the event object
+ *        d - the datum of the node that triggered the mouseup event
+ */
+const addEdge = (e, d) => {
+  const newLink = { source: selectedNodeIndex, target: d.index };
+  links.push(newLink);
+  update();
+
   turnOffAddEdge();
 }
 
 const startAddEdge = (e, d) => {
-  addingEdge = 1;
-  //TODO add filter function here to excluded selected node
-  const selectedNodeId = d.id;
-  console.log("selected Node id", selectedNodeId);
+  selectedNodeIndex = d.id;
+  //add a listener to every node but the current one so that addEdge
+  //is not called when user releases mouse over selected edge
   mySvg.selectAll('.node')
     .filter((d) => {
-      return(selectedNodeId != d.id)
+      return(selectedNodeIndex != d.index)
     })
-      .on('mouseup.addEdge', addEdge);
+    .on('mouseup.addEdge', addEdge);
 }
 
 const turnOffAddNode = () => {
@@ -63,7 +69,7 @@ const turnOnAddNode = () => {
 }
 
 const turnOnAddEdge = () => {
-  node = mySvg.selectAll('.node')
+  mySvg.selectAll('.node')
     .on('mouseup.addEdge', addEdge);
 }
 
@@ -78,114 +84,53 @@ mySvg.on('mouseup', turnOffAddEdge);
 
 // update graph visualization with svg drawings
 const update = () => {
-  node = mySvg.selectAll('.node')
-    .data(force.nodes())
+  edges = mySvg.selectAll('.link').data(links);
+  const edgesEnter = edges
+    .enter().append('line')
+      .attr('x1', function(d) { return d.source.x; })
+      .attr('y1', function(d) { return d.source.y; })
+      .attr('x2', function(d) { return d.target.x; })
+      .attr('y2', function(d) { return d.target.y; })
+      .attr('class', 'link')
+      .attr('stroke', 'white')
+      .attr('stroke-width', '3');
+
+  edges = edgesEnter.merge(edges);
+
+  vertices = mySvg.selectAll('.node').data(nodes);
+  const verticesEnter = vertices
     .enter().append('circle')
-    .attr('class', 'node')
-    .attr('id', (d) => {return(d.id)})
-    .attr('r', nodeValues.radius)
-    .style('fill', (d) => {
-      return colors[d.id % 6];
-    })
-    .on('mousedown', startAddEdge)
-  //turn of listener for add node so that new node is not created
-    .on('mouseover.addNode', turnOffAddNode)
-    .on('mouseout.addNode', turnOnAddNode)
-  force.restart();
+      .attr('cx', function(d) { return d.x; })
+      .attr('cy', function(d) { return d.y; })
+      .attr('class', 'node')
+      .attr('id', (d) => {return(d.id)})
+      .attr('r', nodeValues.radius)
+      .style('fill', (d) => {
+        return colors[d.id % 6];
+      })
+      .on('mousedown', startAddEdge)
+      // turn of listener for add node so that new node is not created
+      .on('mouseover.addNode', turnOffAddNode)
+      .on('mouseout.addNode', turnOnAddNode);
+
+  vertices = verticesEnter.merge(vertices);
+
+  force.nodes(nodes);
+  force.force("link").links(links);
+  force
+    .alpha(1)
+    .restart();
 }
 
 
 const tick = (e) => {
-  node.attr('cx', function(d) {
-    return d.x;
-  });
-  node.attr('cy', function(d) { return d.y; });
+  edges.attr('x1', function(d) { return d.source.x; })
+       .attr('y1', function(d) { return d.source.y; })
+       .attr('x2', function(d) { return d.target.x; })
+       .attr('y2', function(d) { return d.target.y; });
+  vertices.attr('cx', function(d) { return d.x; })
+          .attr('cy', function(d) { return d.y; });
 }
 
 force.on('tick', tick);
-
-
-
-
-
-
-//const tick = (e) => {
-//
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const addVertex = (myEvent) => {
-//  myVertexSet.push(myVertexSet.length+1);
-//  console.debug("myVertexSet", myVertexSet);
-//  const coordinates = d3.pointer(myEvent);
-//  const newVertex = d3.select("g")
-//
-//  newVertex.append("circle")
-//    .attr('cx', coordinates[0])
-//    .attr('cy', coordinates[1])
-//    .attr('r', 20)
-//    .attr('fill', 'white')
-//    .attr('stroke', 'black');
-//  newVertex.append("text")
-//    .attr('x', coordinates[0])
-//    .attr('y', coordinates[1])
-//    .attr('text-anchor', 'middle')
-//    .style('color', 'white')
-//    .html("hello");
-//
-//  mySvg.append(newVertex);
-//}
-
-//mySvg.on("click", addVertex);
-
-
-
-
-
-//const myVertex = mySvg.data(myVertexSet)
-//  .data(myEdgeSet)
-//  .append("g");
-
-
-
-//myVertex.append("circle")
-//  .attr('cx', 50)
-//  .attr('cy', 50)
-//  .attr('r', 20);
-//
-//myVertex.append("text")
-//  .attr('x', "50")
-//  .attr('y', "50")
-//  .attr("text-anchor", "middle")
-//  .html("hello");
 
